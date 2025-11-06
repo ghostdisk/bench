5abfd6598b68ce786c049c67508b1807d29270fa
ghostdisk <ghostdisk@panzermail.com>
2025-11-06 22:01:22 +0200

    Add arena

---
+++ bench/arena.cpp
@@ -0,0 +1,94 @@
+#include <bench/arena.hpp>
+#include <bench/string.hpp>
+
+namespace bench {
+
+static Arena g_frame_arenas[NUM_FRAME_ARENAS] = {};
+static Arena g_scratch_arena;
+static U32 g_current_frame_arena_index = 0;
+
+void InitArenas() {
+	for (U32 i = 0; i < NUM_FRAME_ARENAS; i++) {
+		g_frame_arenas[i].Init();
+	}
+	g_scratch_arena.Init();
+}
+
+void RotateScratchArenas() {
+	g_current_frame_arena_index++;
+	if (g_current_frame_arena_index >= NUM_FRAME_ARENAS)
+		g_current_frame_arena_index = 0;
+}
+
+
+ScratchArenaView::ScratchArenaView(Arena& arena) : arena(arena) {
+	this->reset_point = arena.GetResetPoint();
+}
+
+ScratchArenaView::~ScratchArenaView() {
+	this->arena.Reset(this->reset_point);
+}
+
+ScratchArenaView Arena::Scratch() {
+	return ScratchArenaView(g_scratch_arena);
+}
+
+Arena& Arena::FrameArena() {
+	return g_frame_arenas[g_current_frame_arena_index];
+}
+
+void Arena::Init() {
+	this->start = (U8*)malloc(ARENA_SIZE);
+	this->end = this->start + ARENA_SIZE;
+	this->head = this->start;
+
+	AssertAlways(this->start, "Out of memory");
+}
+
+void Arena::Destroy() {
+	if (this->start) {
+		free(this->start);
+	}
+	*this = {};
+}
+
+void Arena::AlignHead(U32 alignment) {
+	uintptr_t mask = uintptr_t(alignment - 1);
+	this->head = (U8*)((uintptr_t(this->head) + mask) & (~mask));
+}
+
+void* Arena::Allocate(U32 length) {
+	U8* ptr = this->head;
+	this->head += length;
+	AssertAlways(this->head <= this->end, "Arena overflow");
+	return ptr;
+}
+
+void* Arena::Allocate(U32 length, U32 alignment) {
+	this->AlignHead(alignment);
+	return this->Allocate(length);
+}
+
+ArenaResetPoint Arena::GetResetPoint() {
+	return ArenaResetPoint{this->head};
+}
+
+void Arena::Reset(ArenaResetPoint reset_point) {
+	this->head = reset_point.head;
+}
+
+String Arena::InternString(String string) {
+	U8* buffer = (U8*)this->Allocate(string.length);
+	memcpy(buffer, string.data, string.length);
+	return String(buffer, string.length);
+}
+
+char* Arena::InternCString(String string) {
+	U8* buffer = (U8*)this->Allocate(string.length + 1);
+	memcpy(buffer, string.data, string.length);
+	buffer[string.length] = '\0';
+	return (char*)buffer;
+}
+
+
+}
--- bench/arena.hpp
+++ bench/arena.hpp
@@ -1,8 +1,59 @@
 #pragma once
+#include <bench/common.hpp>
+#include <new>
 
 namespace bench {
 
+/**
+ * How many frame arenas are in rotation?
+ * Memory allocated by a frame arena will live for NUM_FRAME_ARENAS frames
+ * Must be at least 2.
+ */
+static constexpr U32 NUM_FRAME_ARENAS = 3;
+
+static constexpr U32 ARENA_SIZE = (1 << 20);
+
+struct String;
+struct ScratchArenaView;
+
+struct ArenaResetPoint {
+	U8* head = nullptr;
+};
+
 struct Arena {
+	U8* start = nullptr;
+	U8* end = nullptr;
+	U8* head = nullptr;
+
+	void Init();
+	void Destroy();
+	void AlignHead(U32 alignment);
+	void* Allocate(U32 length);
+	void* Allocate(U32 length, U32 alignment);
+	ArenaResetPoint GetResetPoint();
+	void Reset(ArenaResetPoint reset_point);
+
+	String InternString(String string);
+	char* InternCString(String string);
+
+	static ScratchArenaView Scratch();
+	static Arena& FrameArena();
+
+	template <typename T, typename ...Args>
+	T* New(Args&& ...args) {
+		T* ptr = (T*)Allocate(sizeof(T), alignof(T));
+		new (ptr) T(static_cast<Args&&>(...args));
+		return ptr;
+	}
+};
+
+class ScratchArenaView {
+public:
+	Arena& arena;
+	ArenaResetPoint reset_point;
+
+	ScratchArenaView(Arena& arena);
+	~ScratchArenaView();
 };
 
 }
